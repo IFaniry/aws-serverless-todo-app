c4dd280eff657b0a6c55353b739750795c2d3c62
IFaniry <faniry.consulting@gmail.com>
2022-10-04 19:21:38 +0300

    set up Auth0 for dev

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,27 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { JwksClient, CertSigningKey, RsaSigningKey } from 'jwks-rsa'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+// import Axios from 'axios'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+// jwksUri is from Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+const jwksUri = 'https://dev-y5vfm-dl.us.auth0.com/.well-known/jwks.json'
+// https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+const jwksClient = new JwksClient({ jwksUri })
+function getKey(header, callback){
+  jwksClient.getSigningKey(header.kid, function(_, key) {
+    const signingKey = (key as CertSigningKey).publicKey || (key as RsaSigningKey).rsaPublicKey
+
+    callback(null, signingKey)
+  });
+}
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,12 +65,23 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+
+  function jwtVerify(token): Promise<JwtPayload> {
+    return new Promise(function (resolve, reject) {
+      verify(token, getKey, { algorithms: ["RS256"] }, function (err, decoded) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(decoded as JwtPayload);
+        }
+      });
+    });
+  }
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  // https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+  return jwtVerify(token)
 }
 
 function getToken(authHeader: string): string {
